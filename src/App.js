708f2dcef29f59d737680bd5f3036729e01b32ea
BryceeL <bcllamc280@gmail.com>
2024-09-18 18:54:29 -0700

    turned parts of code in components
    Components created:
    DataTable.js
    FetchDataButton.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import FetchDataButton from './components/FetchDataButton.js';
+import DataTable from './components/DataTable.js';
+
 function App() {
   const [data, setData] = useState([]);  // State to store the fetched data
 
   // Function to fetch data from Django API
   const fetchData = () => {
-    axios.get('https://ip-172-31-28-218.us-east-2.compute.internal')  // Update with your Django API URL
+    axios.get('http://localhost:8000/api/dummy-data/')  // Update with your Django API URL
       .then((response) => {
         setData(response.data);  // Store the API response in the state
       })
@@ -18,27 +21,13 @@ function App() {
   return (
     <div className="App">
       <h1>Dummy Data from Django</h1>
-      <button onClick={fetchData}>Fetch Data</button>
-
-      {/* Display fetched data in a table */}
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Password</th>  {/* Replace with other fields if necessary */}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.username}</td>
-              <td>{item.password}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <FetchDataButton
+        buttonText="Fetch Data here"
+        API_URL="http://localhost:8000/api/dummy-data/"
+        setDataMethod={setData}
+      ></FetchDataButton>
+      
+      <DataTable data={data}></DataTable>
     </div>
   );
 }
