e83e8f9d632e8373dee7c1c074061ac0b655e149
BryceeL <bcllamc280@gmail.com>
2024-09-19 21:32:04 -0700

    removed update_likes button/function
    turned it into a component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 import FetchDataButton from './components/FetchDataButton.js';
+import PostDataButton from './components/PostDataButton.js';
 import DataTable from './components/DataTable.js';
 
 function App() {
   const [displayedData, setDisplayedData] = useState([])
-  //??
-  const data = {
-    email: 'email@gmial.com'
-  };
-
-  const fetch_update_likes = () => {
-    axios.post('http://localhost:8000/api/update_likes/', data)
-      .then((response) => {
-        console.log(response.data)
-      })
-      .catch((error) => {
-        console.error('Error updating likes:', error);
-      });
-  };
   
   return (
     <div className="App">
@@ -29,12 +16,21 @@ function App() {
         API_URL="http://localhost:8000/api/client-info/"
         setDataMethod={setDisplayedData}
       ></FetchDataButton>
+
       <FetchDataButton
         buttonText="Fetch likes Data"
         API_URL="http://localhost:8000/api/likes/"
         setDataMethod={setDisplayedData}
       ></FetchDataButton>
-      <button onClick={fetch_update_likes}>Update Likes</button>
+
+      <PostDataButton
+        buttonText="Update Likes"
+        API_URL="http://localhost:8000/api/update_likes/"
+        dataToPost={{
+          email: 'email@gmial.com'
+        }}
+      ></PostDataButton>
+      {/* <button onClick={fetch_update_likes}>Update Likes</button> */}
       
       <DataTable data={displayedData}></DataTable>
     </div>
