edc1a9c58ed8b0c163f36c3327bafd553bcb07fb
Alvaro Miranda <xamalvaro@gmail.com>
2024-09-20 11:12:33 -0700

    updated the code to current with the reactworks

--- src/App.js
+++ src/App.js
@@ -11,21 +11,46 @@ function App() {
   return (
     <div className="App">
       <h1>Dummy Data from Django</h1>
+      <h1>Dummy Data from Django</h1>
       <FetchDataButton
-        buttonText="Fetch client-info Data"
-        API_URL="http://localhost:8000/api/client-info/"
+        buttonText="Fetch user data"
+        API_URL="http://3.142.185.208:8000/api/users/"
         setDataMethod={setDisplayedData}
       ></FetchDataButton>
-
-      <FetchDataButton
+      {/*<FetchDataButton
         buttonText="Fetch likes Data"
         API_URL="http://localhost:8000/api/likes/"
+        setDataMethod={setDisplayedData}
+      ></FetchDataButton>*/}
+      <FetchDataButton
+        buttonText="Fetch posts Data"
+        API_URL="http://3.142.185.208:8000/api/posts/"
+        setDataMethod={setDisplayedData}
+      ></FetchDataButton>
+      <FetchDataButton
+        buttonText="Fetch personal_pages data"
+        API_URL="http://3.142.185.208:8000/api/personal_pages/"
+        setDataMethod={setDisplayedData}
+      ></FetchDataButton>
+      <FetchDataButton
+        buttonText="Fetch comments data"
+        API_URL="http://3.142.185.208:8000/api/comments/"
+        setDataMethod={setDisplayedData}
+      ></FetchDataButton>
+      <FetchDataButton
+        buttonText="Fetch replies data"
+        API_URL="http://3.142.185.208:8000/api/replies/"
+        setDataMethod={setDisplayedData}
+      ></FetchDataButton>
+      <FetchDataButton
+        buttonText="Fetch messages data"
+        API_URL="http://3.142.185.208:8000/api/messages/"
         setDataMethod={setDisplayedData}
       ></FetchDataButton>
 
       <PostDataButton
         buttonText="Update Likes"
-        API_URL="http://localhost:8000/api/update_likes/"
+        API_URL="http://3.142.185.208:8000/api/update_likes/"
         dataToPost={{
           email: 'email@gmial.com'
         }}
