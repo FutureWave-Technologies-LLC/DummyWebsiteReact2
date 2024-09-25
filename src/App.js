7fd20dccaaa1b33a542f731427c11960d5147d22
BryceeL <bcllamc280@gmail.com>
2024-09-25 14:18:37 -0700

    remove dummy data; added routes
    moved dummy data and graph to DummyDataPage.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import FetchDataButton from './components/FetchDataButton.js';
-import PostDataButton from './components/PostDataButton.js';
-import DataTable from './components/DataTable.js';
+//Pages
+import DummyDataPage from './pages/DummyDataPage.js';
+import SignUpPage from './pages/SignUpPage.js';
 
 function App() {
-  const [displayedData, setDisplayedData] = useState([])
-  
   return (
-    <div className="App">
-      <h1>Dummy Data from Django</h1>
-      <FetchDataButton
-        buttonText="Fetch user data"
-        API_URL="http://3.142.185.208:8000/api/users/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-      {/*<FetchDataButton
-        buttonText="Fetch likes Data"
-        API_URL="http://localhost:8000/api/likes/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>*/}
-      <FetchDataButton
-        buttonText="Fetch posts Data"
-        API_URL="http://3.142.185.208:8000/api/posts/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-      <FetchDataButton
-        buttonText="Fetch personal_pages data"
-        API_URL="http://3.142.185.208:8000/api/personal_pages/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-      <FetchDataButton
-        buttonText="Fetch comments data"
-        API_URL="http://3.142.185.208:8000/api/comments/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-      <FetchDataButton
-        buttonText="Fetch replies data"
-        API_URL="http://3.142.185.208:8000/api/replies/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-      <FetchDataButton
-        buttonText="Fetch messages data"
-        API_URL="http://3.142.185.208:8000/api/messages/"
-        setDataMethod={setDisplayedData}
-      ></FetchDataButton>
-
-      <PostDataButton
-        buttonText="Update Likes"
-        API_URL="http://3.142.185.208:8000/api/update_likes/"
-        dataToPost={{
-          email: 'email@gmial.com'
-        }}
-      ></PostDataButton>
-      
-      <DataTable data={displayedData}></DataTable>
-    </div>
-  );
+    //Establish routes to be used for navigating
+    //https://www.codeconcisely.com/posts/react-navigation/
+    <Routes>
+          <Route path="/" element={<DummyDataPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+    </Routes>
+  )
 }
 
 export default App;
