7ddd765f0b028414d8b4b387bc9da5389da9d636
BryceeL <bcllamc280@gmail.com>
2024-10-01 21:20:53 -0700

    Create Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import './Sidebar.css'
+import NavigateButton from '../components/NavigateButton.js';
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <NavigateButton
+                buttonText="Home"
+                path="/home"
+                bootstrap="border border-opacity-100"
+            ></NavigateButton>
+            <NavigateButton
+                buttonText="Messages"
+                path="/"
+                bootstrap="border border-opacity-100"
+            ></NavigateButton>
+            <NavigateButton
+                buttonText="Create Post"
+                path="/"
+                bootstrap="border border-opacity-100"
+            ></NavigateButton>
+        </div>
+    )
+}
+
+export default SideBar
