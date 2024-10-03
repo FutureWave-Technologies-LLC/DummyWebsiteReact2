e91da9c12dfd72773bcf2923106acac5f9167787
BryceeL <bcllamc280@gmail.com>
2024-10-02 22:24:22 -0700

    Update PostPage.js

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -2,14 +2,23 @@ import Navbar from "../components/Navbar"
 import SideBar from "../components/Sidebar"
 import NotificationBar from "../components/NotificationBar"
 import Post from "../components/Post"
+import CommentFeed from "../components/CommentFeed"
 
 function PostPage() {
+    //This object is ONLY here to visualize the comment feed
+    //Will be deleted later.
     const testPost = {
         post_id: 0,
         user: "Joe Roe",
         media: "",
         title: "This is a post",
-        description: "My description goes here..."
+        description: "My description goes here.",
+        comments: [
+            {user: "Alice Roe", commentText: "Wow, so cool"},
+            {user: "Alice Roe", commentText: "Wow, so cool"},
+            {user: "Alice Roe", commentText: "Wow, so cool"},
+            {user: "Alice Roe", commentText: "Wow, so cool"},
+        ]
     }
 
     return (
@@ -24,6 +33,9 @@ function PostPage() {
                 title={testPost.title}
                 description={testPost.description}
             ></Post>
+            <CommentFeed
+                commentArray={testPost.comments}
+            ></CommentFeed>
             <NotificationBar></NotificationBar>
         </div>
     )
