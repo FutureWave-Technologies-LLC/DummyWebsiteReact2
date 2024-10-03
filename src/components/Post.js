705587ae4489c5d920b139997d8e7accb0c5357f
BryceeL <bcllamc280@gmail.com>
2024-10-02 22:23:57 -0700

    returns  different posts based on isMini

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,18 +3,29 @@ import "./Post.css"
 function Post(props) {
     const {is_mini, post_id, user, media, title, description} = props
 
-    return (
-        <div className={is_mini ? "mini post" : "main post"} id={post_id}>
-            <h2>{title}</h2>
-            <p>Posted by: {user}</p>
-            <p>{description}</p>
-            {!is_mini && (
-                <div>
-                    *This text shows if it is not a mini post*
+    //Mini Post is a button
+    if (is_mini) {
+        return (
+            <button className="mini post" id={post_id}>
+                <h2>{title}</h2>
+                <p>Posted by: {user}</p>
+                <p>{description}</p>
+            </button>
+        )
+    //Normal post is div container
+    } else {
+        return (
+            <div>
+                <div className="normal post" id={post_id}>
+                    <h2>{title}</h2>
+                    <p>Posted by: {user}</p>
+                    <p>{description}</p>
                 </div>
-            )}
-        </div>
-    )
+            </div>
+            
+        )
+    }
+    
 }
 
 export default Post
