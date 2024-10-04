f3e7dd0d4dafe278cd8f910dae77798d8472a475
BryceeL <bcllamc280@gmail.com>
2024-10-04 09:46:40 -0700

    add log out btn

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,10 @@
 import './Sidebar.css'
-import NavigateButton from '../components/NavigateButton.js';
+import NavigateButton from '../components/NavigateButton.js'
+import { useAuth } from "../hooks/AuthProvider"
 
 function SideBar() {
+    const auth = useAuth();
+
     return (
         <div className="sidebar">
             <NavigateButton
@@ -14,6 +17,10 @@ function SideBar() {
                 path="/create-post"
                 bootstrap="border border-opacity-100"
             ></NavigateButton>
+            <button 
+                onClick={() => auth.logOut()} 
+                className="log-out border border-opacity-100"
+            >Log Out</button>
             <NavigateButton
                 buttonText="Dummy page"
                 path="/"
