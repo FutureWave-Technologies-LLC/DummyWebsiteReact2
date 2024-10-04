29d03997223a3f1bbd9c60ec40a2cd031ad17742
BryceeL <bcllamc280@gmail.com>
2024-10-04 09:46:03 -0700

    used Auth to redirect to home

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -2,34 +2,39 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignInPage.css'; 
 import axios from 'axios';
+import { useAuth } from "../hooks/AuthProvider";
 
 const SignInPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const handleSignIn = () => {
+    const auth = useAuth()
+    const handleSignIn = async (e) => {
+        e.preventDefault()
         axios.post("http://3.142.185.208:8000/api/login_page/", {
             username: username,
             password: password,
-    })
-    .then((response) => {
-        if (response.data.error == true){
-            console.log(response.data.response)
-            setError(response.data.response)
-        }
-        else {
-            window.location.href = 'http://18.222.224.80:3000/home/'
-        }
-    })
-    .catch((error) => {
-        console.error('Error posting data:', error);
-    });
-}
+        })
+        .then((response) => {
+            if (response.data.error === true){
+                console.log(response.data.response)
+                setError(response.data.response)
+            }
+            else {
+                //login successful
+                auth.loginAction({username, password})
+                return
+            }
+        })
+        .catch((error) => {
+            console.error('Error posting data:', error);
+        });
+    }
 
     const queryParams = new URLSearchParams(window.location.search)
     const term = queryParams.get("response")
-    console.log(term)
+    // console.log(term)
     return (
         <div className="signin-container">
             {term &&<h3>User Created!</h3>}
