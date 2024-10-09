6d672ce70ef8a7deaee2993c585c3fe26ef80562
BryceeL <bcllamc280@gmail.com>
2024-10-08 20:55:12 -0700

    Update PrivateRoute.js

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/AuthProvider";
 
 // Handles authentication. Uses AuthProvider hook to get user token.
-// If no user token -> redirected to login page
+// If no user token in localStorage -> redirected to login page
 // Else -> user access protected routes; render child components 
 // nested within PrivatRoute componenet accessed via <Outlet />
 const PrivateRoute = () => {
