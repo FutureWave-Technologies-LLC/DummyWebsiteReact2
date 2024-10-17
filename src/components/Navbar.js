abdec50314d3d94a05870a2e869e5bf57b91ba21
BryceeL <bcllamc280@gmail.com>
2024-10-16 21:36:51 -0700

    sets SearchParams that SearchPage.js can use

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,43 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import React, {useState } from 'react';
+import { useAuth } from "../hooks/AuthProvider"
+
+
 import './Navbar.css'
 
-import { useAuth } from "../hooks/AuthProvider"
-import { Button } from 'bootstrap'
-import NavigateButton from '../components/NavigateButton.js'
-
 function Navbar() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [searchQuery, setSearchQuery] = useState(searchParams.get("q") || "");
+    const navigate = useNavigate()
+
     const token = JSON.parse(localStorage.getItem("future-token"))
     const user = useAuth()
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSearchParams({ q: searchQuery })
+        navigate(`/search?q=${encodeURIComponent(searchQuery)}`)
+        window.location.reload();
+        
+    }
+
     return (
-        <div className="navbar">
+        <form className="navbar" onSubmit={handleSubmit}>
             <h2>New Space</h2>
             <input
                 className="search-bar"
-                placeholder="Search">
-            </input>
-            <NavigateButton
-                buttonText="Search"
-                path="/search"
-                bootstrap="border border-opacity-100"
-            ></NavigateButton>
+                placeholder="Search"
+                onChange={(event) => setSearchQuery(event.target.value)}
+            ></input>
+            <button 
+                className="search-btn" 
+                type="submit"
+            >Search</button>
             { user && (
                 <h2>Hello, {token.username}!</h2>
             )}
             
-        </div>
+        </form>
     )
 }
 
