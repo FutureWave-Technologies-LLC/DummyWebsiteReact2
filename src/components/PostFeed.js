017d443b2731b5a655fbe18b27c2bcf31790d636
BryceeL <bcllamc280@gmail.com>
2024-10-16 21:39:25 -0700

    fixed scrollbar to be at top

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import axios from 'axios';
+import axios from 'axios'
+
 import Post from "./Post"
 
 import "./PostFeed.css"
@@ -8,21 +9,16 @@ function PostFeed() {
   var [hasFetchedData, setHasFetchedData] = useState(false)
   const [postFeed, setPostFeed] = useState([])
 
-  const fetchPostData = () => {
-    //Fetch data once; don't call API multiple times
-    if (hasFetchedData == false) {
-      setHasFetchedData(true)
-      axios.get("http://3.142.185.208:8000/api/posts/")  // Update with your Django API URL
-      .then((response) => {
-        setPostFeed(response.data);  // Store the API response in the state
-      })
-      .catch((error) => {
-          console.error('Error fetching post data:', error);
-      });
-    }
-  };
-
-  fetchPostData()
+  if (hasFetchedData === false) {
+    setHasFetchedData(true)
+    axios.get("http://localhost:8000/api/posts/")  
+    .then((response) => {
+      setPostFeed(response.data)
+    })
+    .catch((error) => {
+        console.error('Error fetching post data:', error);
+    })
+  }
 
   return (
       <div className="feed-container">
@@ -30,12 +26,12 @@ function PostFeed() {
               <Post
                   is_mini={true}
                   post_id={post.post_id}
-                  user={post.username}
+                  username={post.username}
                   media={post.media}
                   title={post.title}
                   description={post.text}
               ></Post>
-          ))}
+          )).reverse()}
       </div> 
   )
 }
