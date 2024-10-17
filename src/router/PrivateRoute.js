422fd6c14a8e1fd9d912062744a8ed4db95631a7
BryceeL <bcllamc280@gmail.com>
2024-10-16 21:41:25 -0700

    Update PrivateRoute.js

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useAuth } from "../hooks/AuthProvider";
 // Handles authentication. Uses AuthProvider hook to get user token.
 // If no user token in localStorage -> redirected to login page
 // Else -> user access protected routes; render child components 
-// nested within PrivatRoute componenet accessed via <Outlet />
+// nested within PrivatRoute component accessed via <Outlet />
 const PrivateRoute = () => {
   const user = useAuth()
   if (!user.token) return <Navigate to="/sign-in" />
