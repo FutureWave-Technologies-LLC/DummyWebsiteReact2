030589c73c2c5418e7f7663947cd6c7d38f77ccc
BryceeL <bcllamc280@gmail.com>
2024-10-19 12:31:33 -0700

    useEffect to run api call once

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,19 +1,17 @@
-import { useNavigate, useSearchParams } from "react-router-dom"
-import React, {useState } from 'react';
+import { useSearchParams } from "react-router-dom"
+import React, {useState, useEffect } from 'react';
 import Navbar from "../components/Navbar"
 import SideBar from "../components/Sidebar"
 import NotificationBar from "../components/NotificationBar"
 import axios from 'axios'
 
 function Search() {
-    var [hasSearched, setHasSearched] = useState(false)
     const [searchParams, setSearchParams] = useSearchParams()
     const [queryResult, setQueryResult] = useState([])
 
     const q = searchParams.get('q')
 
-    if (hasSearched == false) {
-        setHasSearched(true)
+    useEffect(() => {
         axios.get("http://localhost:8000/api/search_users/", {
             params: {
                 query: q,
@@ -25,13 +23,14 @@ function Search() {
         .catch((error) => {
             console.error('Error getting data:', error);
         })
-    }
+    }, [])
       
     return (
-        <div className="home-page">
+        <div>
             <Navbar></Navbar>
             <SideBar></SideBar>
             {(!queryResult.error && ( queryResult.map(user => (
+                //TODO: COMPONENT WITH FOLLOW BUTTON
                 <p>{user.username}</p>
             )))) || <p>{queryResult.Response}</p>}
 
