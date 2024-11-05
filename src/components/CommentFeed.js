860aff0d69664bc363e240992bd963546e454f4c
BryceeL <bcllamc280@gmail.com>
2024-11-05 11:43:03 -0800

    displays 'no comments'

--- src/components/CommentFeed.js
+++ src/components/CommentFeed.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import "./CommentFeed.css"
+import "../css/components/CommentFeed.css"
 
 function CommentFeed(props) {
     const {commentFeed, openModalSetter} = props
@@ -8,14 +8,16 @@ function CommentFeed(props) {
     return (
         <div className="comments-container">
             <div className="comments-feed">
-                {commentFeed.map(comment => (
+                {commentFeed.length > 0 ? (commentFeed.map(comment => (
                     <div className="comment">
                         <h5>
                             <Link to={"/profile/"+comment.user_id}>{comment.username}</Link>
                         </h5>
                         <p>{comment.comment}</p>
                     </div>
-                )).reverse()}
+                )).reverse()) : (
+                    <p>No comments.</p>
+                )}
             </div> 
             <button className="comment-btn" onClick={openModalSetter}>Comment</button>
         </div>
