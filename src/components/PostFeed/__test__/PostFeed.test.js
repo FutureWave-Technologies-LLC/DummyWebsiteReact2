3a8b3af34f5f9eaa10bb51a0592fef16f18d3ec3
BryceeL <bcllamc280@gmail.com>
2024-11-22 11:39:11 -0800

    Create PostFeed.test.js

---
+++ src/components/PostFeed/__test__/PostFeed.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import PostFeed from '../PostFeed'
+
+// test('renders posts for a user', async () => {
+//     render(<PostFeed/>)
+//     const posts = screen.getAllByText()
+// })
