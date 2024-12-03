26d1e6b70c1c00adad104c6a9f0444a8607172ed
BryceeL <bcllamc280@gmail.com>
2024-12-03 10:12:54 -0800

    placeholder prop

--- src/components/SettingsComponent/SettingsComponent.js
+++ src/components/SettingsComponent/SettingsComponent.js
@@ -1,12 +1,12 @@
 import './SettingsComponent.css';
 
 function SettingsComponent(props) {
-    const {title, onChange, value, disabled} = props
+    const {title, onChange, value, disabled, placeholder} = props
     return (
         <div className="setting">
             <h3>{title}</h3>
             <input className="settings-input" 
-                placeholder='"https://via.placeholder.com/300"'
+                placeholder={placeholder}
                 onChange={onChange}
                 value={value}
                 disabled={disabled}
