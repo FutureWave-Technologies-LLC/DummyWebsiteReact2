274267cdafa553de760f0172e35a0656349acecf
BryceeL <bcllamc280@gmail.com>
2024-12-02 22:54:44 -0800

    css and bootstrap updates

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -8,47 +8,44 @@ function SideBar() {
     const auth = useAuth();
 
     return (
-        <div className="sidebar">
+        <div className="sidebar newspace-border">
             <NavigateButton
                 buttonText="Home"
                 path="/home"
-                bootstrap="border border-opacity-100"
+                bootstrap="rounded"
                 iconClass="uil uil-home"
             />
             <NavigateButton
                 buttonText="Your Profile"
                 path={`/profile/${token.user_id}`}
-                bootstrap="border border-opacity-100"
+                bootstrap="rounded"
                 iconClass="uil uil-user"
             />
             <NavigateButton
                 buttonText="Messages"
                 path="/messages"
-                bootstrap="border border-opacity-100"
+                bootstrap="rounded"
                 iconClass="uil uil-envelope"
             />
             <NavigateButton
                 buttonText="Create Post"
                 path="/create-post"
-                bootstrap="border border-opacity-100"
+                bootstrap="rounded"
                 iconClass="uil uil-plus-circle"
             />
             <NavigateButton
                 buttonText="User Settings"
                 path="/settings"
-                bootstrap="border border-opacity-100"
+                bootstrap="rounded"
                 iconClass="uil uil-cog"
             />
-            <button 
-                onClick={() => auth.logOut()} 
-                className="log-out border border-opacity-100"
-            >
+            <button onClick={() => auth.logOut()} className="log-out rounded sub1-button">
                 <i className="uil uil-sign-out-alt"></i> Log Out
             </button>
             <NavigateButton
                 buttonText="Dummy page"
                 path="/dum"
-                bootstrap="border border-opacity-100 bg-danger"
+                bootstrap="bg-danger rounded"
                 iconClass="uil uil-page"
             />
         </div>
