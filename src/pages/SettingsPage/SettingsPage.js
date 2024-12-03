433071f556440beeaa3208b462d91e9e9707838b
BryceeL <bcllamc280@gmail.com>
2024-12-02 22:57:20 -0800

    added settings to change banner and profile desc

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 import Navbar from "../../components/Navbar/Navbar";
 import SideBar from "../../components/Sidebar/Sidebar";
-// import Modal from '../../components/Modal/Modal';
+import SettingsComponent from '../../components/SettingsComponent/SettingsComponent';
 
 import './SettingsPage.css';
 
 function SettingsPage() {
-    const [image, setImage] = useState("")
-    const [disableInput, setDisabledInput] = useState(true)
+    const [profileImage, setProfileImage] = useState("")
+    const [bannerImage, setBannerImage] = useState("")
+    const [profileDesc, setProfileDesc] = useState("")
 
+    const [disableInput, setDisabledInput] = useState(true)
     const [notification, setNotification] = useState("")
 
     // Retrieve the token from localStorage
@@ -17,11 +20,13 @@ function SettingsPage() {
 
     useEffect(() => {
         // Fetch user's data
-        axios.get("http://localhost:8000/users/get_user_data/", {
+        axios.get("http://3.17.148.157:8000/users/get_user_data/", {
             params: { user_id: token.user_id },
         })
         .then((response) => {
-            setImage(response.data.profile_image)
+            setProfileImage(response.data.profile_image)
+            setBannerImage(response.data.banner_image)
+            setProfileDesc(response.data.profile_description)
 
             setDisabledInput(false)
         })
@@ -32,11 +37,12 @@ function SettingsPage() {
 
     const submitSettingsChange = async (e) => {
         e.preventDefault();
-        console.log(image)
         try {
-            await axios.post('http://localhost:8000/users/update_settings/', {
+            await axios.post('http://3.17.148.157:8000/users/update_settings/', {
                 user_id: token.user_id,
-                profile_image: image,
+                profile_image: profileImage,
+                banner_image: bannerImage,
+                profile_description: profileDesc,
             })
             setNotification('Settings successfully updated.');
         } catch (error) {
@@ -50,18 +56,30 @@ function SettingsPage() {
             <Navbar />
             <SideBar />
             <form className="settings-form" onSubmit={submitSettingsChange}>
-                <div className="profile-image-container">
-                    <h3>Profile Image</h3>
-                    <input className="settings-input" 
-                        placeholder='"https://via.placeholder.com/300"'
-                        onChange={e=>{setImage(e.target.value)}}
-                        value={image}
+                {notification && <div className="notification">{notification}</div>}
+                <div className="settings-container">
+                    <SettingsComponent
+                        title={"Profile Image"}
+                        onChange={e=>{setProfileImage(e.target.value)}}
+                        value={profileImage}
                         disabled={disableInput}
-                    ></input>
+                    ></SettingsComponent>
+                    <SettingsComponent
+                        title={"Banner Image"}
+                        onChange={e=>{setBannerImage(e.target.value)}}
+                        value={bannerImage}
+                        disabled={disableInput}
+                    ></SettingsComponent>
+                    <SettingsComponent
+                        title={"Profile Description"}
+                        onChange={e=>{setProfileDesc(e.target.value)}}
+                        value={profileDesc}
+                        disabled={disableInput}
+                    ></SettingsComponent>
                 </div>
                 <button className="submit-btn" type="form">Save</button>
             </form>
-            {notification && <div className="notification">{notification}</div>}
+           
         </div>
     )
 }
