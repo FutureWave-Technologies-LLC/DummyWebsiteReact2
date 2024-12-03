4365d1c3c03b02bc9cf00242be28679c3da86d21
BryceeL <bcllamc280@gmail.com>
2024-12-03 10:14:21 -0800

    modularized follow and message button into components

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import { useNavigate, useSearchParams, Link } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -6,13 +6,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import SideBar from "../../components/Sidebar/Sidebar";
 import UserImage from "../../components/UserImage/UserImage";
 import ProfileButton from "../../components/ProfileButton/ProfileButton";
+import FollowButton from '../../components/FollowButton/FollowButton';
+import MessageButton from "../../components/MessageButton/MessageButton";
 
 import './SearchPage.css';
 
 function Search() {
     const [searchParams] = useSearchParams();
     const [queryResult, setQueryResult] = useState([]);
-    const [followingStatus, setFollowingStatus] = useState([]);
 
     const navigate = useNavigate();
 
@@ -27,43 +28,11 @@ function Search() {
         })
         .then((response) => {
             setQueryResult(response.data)
-
-            // Get users and their IDs that this user is following and set to followingStatus state
-            axios.get("http://3.17.148.157:8000/profiles/following/", {
-                params: {
-                    user_id: token.user_id,
-                }
-            })
-            .then(response => {
-                response.data.forEach(following_user => {
-                    setFollowingStatus(prevState => [...prevState, following_user.username]);
-                });
-            })
-            .catch(err => console.error("Error fetching ID:"+token.user_id+" followers:", err));
-        
         })
         .catch((error) => {
             console.error('Error fetching query result:', error);
         });
     }, []);
-
-    // Handles Follow and Unfollow
-    function toggleFollow(usernameToFollow) {
-        axios.post("http://3.17.148.157:8000/profiles/following/", {
-            followee_username: usernameToFollow,
-            follower_id: token.user_id,
-        })
-        .then(response => {
-            if (response.data.Followed) {
-                setFollowingStatus(prevState => [...prevState, usernameToFollow]);
-            } else {
-                setFollowingStatus(prevState => prevState.filter(i => i !== usernameToFollow));
-            }
-        })
-        .catch(error => {
-            console.error('Error following/unfollowing user:', error);
-        });
-    }
 
     // Navigate to messages page to start a conversation
     function navigateToMessages(user) {
@@ -86,12 +55,14 @@ function Search() {
                             ></ProfileButton> 
                             {user.username !== token.username && (
                                 <div>
-                                    <button className="border sub1-button" onClick={() => toggleFollow(user.username)}>
-                                        {followingStatus.includes(user.username) ? 'Unfollow' : 'Follow'}
-                                    </button>
-                                    <button className="border sub1-button" onClick={() => navigateToMessages(user)}>
-                                        Message
-                                    </button>
+                                    <FollowButton
+                                        user_id={token.user_id}
+                                        followee_username={user.username}
+                                        followee_id={user.user_id}
+                                    ></FollowButton>
+                                    <MessageButton
+                                        userToMessage={user}
+                                    ></MessageButton>
                                 </div>
                             )}
                         </div>
