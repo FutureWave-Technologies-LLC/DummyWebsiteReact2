c53b45bb055758066e1ec6800f8a3bc010df6c5f
BryceeL <bcllamc280@gmail.com>
2024-12-11 12:18:52 -0800

    removed commented code

--- src/components/NavigateButton/NavigateButton.js
+++ src/components/NavigateButton/NavigateButton.js
@@ -23,29 +23,3 @@ function NavigateButton(props) {
 }
 
 export default NavigateButton;
-
-// import { useNavigate } from 'react-router-dom';
-
-// function NavigateButton(props) {
-//   const {buttonText, path, bootstrap} = props
-  
-//   const navigate = useNavigate();
-
-//   const handleClick = () => {
-//     if (path.includes("https://")) {
-//       window.location.href = path
-//     } else {
-//       navigate(path);
-//       window.location.reload();
-//     }
-      
-//   };
-
-//   return (
-//     <button className={bootstrap} onClick={handleClick}>
-//           {buttonText}
-//     </button>
-//   )
-// }
-
-// export default NavigateButton;
