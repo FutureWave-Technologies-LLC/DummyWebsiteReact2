ce31b681a4e5baa12e19698e984d2b6560de4999
BryceeL <bcllamc280@gmail.com>
2024-12-11 18:26:36 -0800

    removed commented code

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,5 +1,3 @@
-import React, {useEffect, useState } from 'react';
-import axios from 'axios';
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/AuthProvider";
 
@@ -9,32 +7,9 @@ import { useAuth } from "../hooks/AuthProvider";
 // nested within PrivatRoute component accessed via <Outlet />
 const PrivateRoute = () => {
   const user = useAuth()
-  // const [authToken, setAuthToken] = useState()
-  
-  // useEffect(() => {
-  //   if (user.token) {
-  //     const token = JSON.parse(localStorage.getItem("future-token"))
-  //     axios.get("http://3.17.148.157:8000/users/compare_tokens/", {
-  //       params: {
-  //           user_id: token.user_id,
-  //           token_id: token.token_id,
-  //       }
-  //     })
-  //     .then((response) => {
-  //       setAuthToken(response.data.response)
-  //     })
-  //     .catch((error) => {
-  //         console.error('Error comparing token ids:', error);
-  //     });
-  //   }
-  // }, [])
-  
+
   if (user.token) {
     return <Outlet />
-    // if (authToken === true) {
-    //   return <Outlet />
-    // }
-    // else return <Navigate to="/login" />
 
   } else {
       return <Navigate to="/login" />
