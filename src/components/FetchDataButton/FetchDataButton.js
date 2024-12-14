b94cc48cf4c24911b74d45b8ac03408f2bc05b87
BryceeL <bcllamc280@gmail.com>
2024-12-13 21:33:21 -0800

    accepts params

--- src/components/FetchDataButton/FetchDataButton.js
+++ src/components/FetchDataButton/FetchDataButton.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 // API_URL: Django API URL used to fetch data with
 // setDataMethod: useState's setterMethod
 function FetchDataButton(props) {
-    const {buttonText, API_URL, setDataMethod} = props
+    const {buttonText, API_URL, setDataMethod, params} = props
 
     // Function to fetch data from Django API
     const fetchData = () => {
-        axios.get(API_URL)  // Update with your Django API URL
+        axios.get(API_URL, {
+            params: params
+        })  // Update with your Django API URL
         .then((response) => {
             setDataMethod(response.data);  // Store the API response in the state
         })
