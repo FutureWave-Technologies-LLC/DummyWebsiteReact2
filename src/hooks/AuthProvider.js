92012dd794de55800b9b2cc8850cf2802e24c148
ayecmon <112906516+ayecmon@users.noreply.github.com>
2025-03-03 20:27:34 -0800

    bruh

--- src/hooks/AuthProvider.js
+++ src/hooks/AuthProvider.js
@@ -12,7 +12,7 @@ const AuthProvider = ({ children }) => {
 
     const loginAction = (input) => {
       //use username and password to authenticate
-      axios.post("https://18.217.60.48:8000/users/authenticate_user/", {
+      axios.post("http://18.191.115.9:8000/users/authenticate_user/", {
         username: input.username,
         password: input.password
       })
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -22,7 +22,7 @@ const LoginPage = () => {
 
     const handleSignIn = async (e) => {
         e.preventDefault()
-        axios.get("https://18.217.60.48:8000/users/login/", {
+        axios.get("http://18.191.115.9:8000/users/login/", {
             params: {
                 username: username,
                 password: password,
